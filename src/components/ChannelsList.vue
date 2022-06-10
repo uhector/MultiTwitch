313f7db287f3ca2f7dd5e0b6e57b84748c836c3f
Hector Ulacio <hectorulacior@gmail.com>
2022-06-10 01:37:27 -0400

    create ChannelsList component

---
+++ src/components/ChannelsList.vue
@@ -0,0 +1,36 @@
+<script setup>
+import ChannelsListItem from './ChannelsListItem.vue'
+
+defineProps({
+  channels: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <ul class="channels-list">
+    <!-- ChannelsListItem -->
+    <ChannelsListItem
+      v-for="channel, index in channels"
+      @delete-channel="$emit('delete-channel', $event)"
+      :index="index">
+      {{ channel }}
+    </ChannelsListItem>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+.channels-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
